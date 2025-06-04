127f0035ad05056359fee44b356c93edbf38bdbf
Shekhar Gadge <136042299+Shekhar-PythonDev@users.noreply.github.com>
2025-06-04 14:21:33 +0530

    Add files via upload

---
+++ Revers.py
@@ -0,0 +1,25 @@
+#perform negetive/revers slicing and indexing on the string "INSTAGRAM"
+s = "INSTAGRAM"
+
+# Negative indexing
+# Last character
+print(s[-1])
+print(s[-2])
+print(s[-3])
+print(s[-4])
+print(s[-5])
+print(s[-6])
+print(s[-7])
+print(s[-8])
+
+# Reverse slicing
+print(s[::-1])
+print(s[::-2])
+print(s[::-3])
+print(s[::-4])
+print(s[::-5])
+print(s[::-6])
+print(s[::-7])
+print(s[::-8])
+print(s[::-9])
+print(s[-7:-9:-3])
