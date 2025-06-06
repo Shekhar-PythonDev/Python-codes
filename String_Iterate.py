4e0cb9cff01680eaa3376abd525ccc8a3b04c8c1
Shekhar Gadge <136042299+Shekhar-PythonDev@users.noreply.github.com>
2025-06-06 16:32:40 +0530

    Add files via upload

---
+++ String_Iterate.py
@@ -0,0 +1,90 @@
+# How to Iterate through a string in Python?
+
+# s = "INSTAGRAM"
+# print("Start of for loop")
+# for v in s:
+#     print(v)
+# print("End of for loop")
+
+# Find the freq of Char a in given string
+# s = "INSTAGRAM"
+# count = 0
+
+# print("Start of for loop")
+
+# for v in s:
+
+#  if v == 'A':
+#       count = count + 1
+# print("Frequency of Char A is: ", count)
+
+
+# print("End of for loop")
+
+# #s = "INSTAGRAM AND FACEBOOK" 
+# count = 0
+# print("Start of for loop")
+# # for i in range(0,len(s),2):
+# for i in range(0,len(s),3): 
+#  print(i,"---->", s[i])
+# print("End of for loop")
+
+
+#String
+
+# s="MY NAME IS SHEKHAR GADGE"
+# count=0
+# # print("Start of for loop")
+# for i in s:
+
+#     if i == " ":
+#         count = count + 1
+# print("Frequency of Space is: ", count)
+
+
+# print("END of for loop")
+
+# s="My Name Is Shekhar Gadge"
+# vowels = "aieou"
+# count = 0
+# for i in "s":
+#     i = s.lower() # Convert to uppercase to match with vowels
+#     if i in vowels:
+#         count = count + 1
+# print("Total number of spaces in given str= ", count)
+
+# s="My Name Is Shekhar Gadge"
+# vowels = "aieou"
+# count = 0
+# for i in s.lower():
+#     if i in vowels:
+#         count = count + 1
+# print("Total number of vowels in given str= ", count)
+
+#Printing reverse of string
+
+# s="My Name Is Shekhar Gadge"
+# print("orignal string is: ", s)
+# # rev_s = ""
+# # for i in s:
+# #     rev_s = i + rev_s
+# print( s[::-1])
+
+# rev2_s =""
+# for i in range(len(s)-1, -1, -1):
+#      rev2_s = rev2_s + s[i]
+# print("Reverse of string is: ", rev2_s)
+
+# rev3_s =""
+# for i in range(0,len(s)):
+#     rev3_s = s[len(s)-1-i] + rev3_s
+# print("Reverse of string is: ", rev3_s)
+
+#String methods 
+#isalpha(), isdigit(), isalnum(), isspace(), islower(), isupper(), istitle()
+
+
+    
+    
+    
+
